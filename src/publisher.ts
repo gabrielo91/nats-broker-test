cddbd0e147391c2e6098e919aa562616fa45ce11
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-11-23 01:01:15 -0500

    publisher improved

--- src/publisher.ts
+++ src/publisher.ts
@@ -7,10 +7,10 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Connected to NATS services');
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
+  await publisher.publish({
     id: '12',
     title: 'concert',
     price: 20,
